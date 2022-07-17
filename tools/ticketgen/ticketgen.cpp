30c2b10d6cd4b974ec493fe492d7c3b3d1260fd7
yourmnbbn <584531339@qq.com>
2022-07-18 00:07:57 +0800

    Add linux output for ticketgen

--- tools/ticketgen/ticketgen.cpp
+++ tools/ticketgen/ticketgen.cpp
@@ -31,19 +31,35 @@ int main(int argc, char** argv)
     unsigned int keysize = 0;
 
     SteamUser()->GetAuthSessionTicket(ticket, STEAM_KEYSIZE, &keysize);
-    snprintf(filename, sizeof(filename), "%llu.txt", SteamUser()->GetSteamID().ConvertToUint64());
+    snprintf(filename, sizeof(filename), "%llu_windows.txt", SteamUser()->GetSteamID().ConvertToUint64());
 
     std::ofstream file_out(filename, std::ofstream::out);
     if (file_out.bad() || !file_out.is_open())
         return -1;
 
-    printf("The Auth Ticket is as follow:\n");
-
+    printf("The Auth Ticket is as follow:\n\n");
+    printf("windows command line:\n-ticket ");
     char temp[8];
     for (size_t i = 0; i < keysize; i++)
     {
         printf("\\x%02X", ticket[i] & 0xFF);
         snprintf(temp, sizeof(temp), "\\x%02X", ticket[i] & 0xFF);
+        file_out << temp;
+    }
+
+    file_out.close();
+
+    printf("\nLinux command line:\n-ticket ");
+    snprintf(filename, sizeof(filename), "%llu_linux.txt", SteamUser()->GetSteamID().ConvertToUint64());
+
+    file_out.open(filename, std::ofstream::out);
+    if (file_out.bad() || !file_out.is_open())
+        return -1;
+    
+    for (size_t i = 0; i < keysize; i++)
+    {
+        printf("\\\\x%02X", ticket[i] & 0xFF);
+        snprintf(temp, sizeof(temp), "\\\\x%02X", ticket[i] & 0xFF);
         file_out << temp;
     }
 
