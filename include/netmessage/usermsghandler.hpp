2337cf715da831ebadcaa347f58a18f871dfa4ed
yourmnbbn <584531339@qq.com>
2022-07-27 14:34:57 +0800

    Handle more user messages

--- include/netmessage/usermsghandler.hpp
+++ include/netmessage/usermsghandler.hpp
@@ -5,6 +5,7 @@
 #pragma once
 #endif
 
+#include <ranges>
 #include "cstrike15_usermessages.pb.h"
 #include "../common/bitbuf.h"
 
@@ -16,6 +17,7 @@ public:
 private:
 	template<typename MsgObjType>
 	static MsgObjType usermsg_cast(const void* data, size_t length);
+	static const std::string& RemoveColorCode(const std::string& message);
 };
 
 template<typename MsgObjType>
@@ -24,6 +26,13 @@ MsgObjType CUserMsgHandler::usermsg_cast(const void* data, size_t length)
 	MsgObjType obj;
 	obj.ParseFromArray(data, length);
 	return obj;
+}
+
+const std::string& CUserMsgHandler::RemoveColorCode(const std::string& message)
+{
+	auto [first, end] = std::ranges::remove_if(const_cast<std::string&>(message), [](char c) {return c > 0 && c <= 0x0F; });
+	const_cast<std::string&>(message).erase(first, end);
+	return message;
 }
 
 bool CUserMsgHandler::HandleUserMessage(int msgType, const void* data, size_t length)
@@ -46,7 +55,7 @@ bool CUserMsgHandler::HandleUserMessage(int msgType, const void* data, size_t le
 	{
 		auto sayText = usermsg_cast<CCSUsrMsg_SayText>(data, length);
 		int client = sayText.ent_idx();
-		printf("SayText: <Client %d> %s\n", client, sayText.text().c_str());
+		printf("SayText: <Client %d> %s\n", client, RemoveColorCode(sayText.text()).c_str());
 		return true;
 	}
 	case CS_UM_SayText2:
@@ -54,7 +63,10 @@ bool CUserMsgHandler::HandleUserMessage(int msgType, const void* data, size_t le
 		auto sayText2 = usermsg_cast<CCSUsrMsg_SayText2>(data, length);
 		int client = sayText2.ent_idx();
 		printf("SayText2<type: %s> Playere: %s<index %d> : %s\n", 
-			sayText2.msg_name().c_str(), sayText2.params(0).c_str(), client, sayText2.params(1).c_str());
+			RemoveColorCode(sayText2.msg_name()).c_str(), RemoveColorCode(sayText2.params(0)).c_str(), 
+			client, RemoveColorCode(sayText2.params(1)).c_str());
+		auto var = sayText2.params(0);
+		
 		return true;
 	}
 	case CS_UM_TextMsg:
@@ -63,7 +75,7 @@ bool CUserMsgHandler::HandleUserMessage(int msgType, const void* data, size_t le
 		for (int i = 0; i < 5; ++i)
 		{
 			if(textMsg.params(i).c_str()[0])
-				printf("%s\n", textMsg.params(i).c_str());
+				printf("%s\n", RemoveColorCode(textMsg.params(i)).c_str());
 		}
 		return true;
 	}
@@ -77,13 +89,33 @@ bool CUserMsgHandler::HandleUserMessage(int msgType, const void* data, size_t le
 		{
 			printf("ShowMenu<time: %ds> \n%s\n", displayTime, menu.menu_string().c_str());
 		}
-
-	}
-	case CS_UM_ProcessSpottedEntityUpdate:
-	{
-		//Do nothing
 		return true;
 	}
+	case CS_UM_MatchEndConditions:
+	{
+		auto matchEndCondEvent = usermsg_cast<CCSUsrMsg_MatchEndConditions>(data, length);
+		printf("Event match_end_conditions:\n\tfrags: %d\n\tmax_rounds: %d\n\twin_rounds: %d\n\ttime: %d\n\n",
+			(int)matchEndCondEvent.fraglimit(), (int)matchEndCondEvent.mp_maxrounds(),
+			(int)matchEndCondEvent.mp_winlimit(), (int)matchEndCondEvent.mp_timelimit());
+		return true;
+	}
+	case CS_UM_HintText:
+	{
+		//hint is sometimes annoying in our client
+		//auto hintText = usermsg_cast<CCSUsrMsg_HintText>(data, length);
+		//printf("Hint text: %s", hintText.text().c_str());
+		return true;
+	}
+	case CS_UM_StopSpectatorMode:
+	case CS_UM_KillCam:
+	case CS_UM_Fade:
+	case CS_UM_Rumble:
+	case CS_UM_ResetHud:
+	case CS_UM_Train:
+	case CS_UM_RequestState:
+	case CS_UM_ProcessSpottedEntityUpdate:
+		//Do nothing
+		return true;
 	default: return false;
 	}
 }
